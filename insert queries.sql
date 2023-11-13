7ca995bdfc64a9f5dc22ba5fc91bec241d3507b4
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2023-11-13 13:12:43 +0200

    Create insert queries.sql

---
+++ insert queries.sql
@@ -0,0 +1,29 @@
+INSERT INTO users (first_name, last_name, user_email, registration_date, password)
+VALUES ('John', 'Doe', 'john.doe@email.com', '2020-01-15', 'password123');
+
+INSERT INTO users (first_name, last_name, user_email, registration_date, password)
+VALUES ('Alice', 'Smith', 'alice.smith@email.com', '2020-03-22', 'securepassword');
+
+INSERT INTO users (first_name, last_name, user_email, registration_date, password)
+VALUES ('Bob', 'Johnson', 'bob.johnson@email.com', '2021-05-10', 'musiclover123');
+
+INSERT INTO users (first_name, last_name, user_email, registration_date, password)
+VALUES ('Emma', 'Williams', 'emma.williams@email.com', '2021-08-07', 'password456');
+
+INSERT INTO users (first_name, last_name, user_email, registration_date, password)
+VALUES ('David', 'Miller', 'david.miller@email.com', '2022-02-28', 'mysecretpass');
+
+INSERT INTO users (first_name, last_name, user_email, registration_date, password)
+VALUES ('Olivia', 'Davis', 'olivia.davis@email.com', '2022-06-12', '1234olivia');
+
+INSERT INTO users (first_name, last_name, user_email, registration_date, password)
+VALUES ('James', 'Taylor', 'james.taylor@email.com', '2023-09-05', 'taylorjam');
+
+INSERT INTO users (first_name, last_name, user_email, registration_date, password)
+VALUES ('Sophia', 'Brown', 'sophia.brown@email.com', '2023-11-18', 'brownie456');
+
+INSERT INTO users (first_name, last_name, user_email, registration_date, password)
+VALUES ('William', 'Moore', 'william.moore@email.com', '2020-07-03', 'password789');
+
+INSERT INTO users (first_name, last_name, user_email, registration_date, password)
+VALUES ('Ava', 'Clark', 'ava.clark@email.com', '2021-12-30', 'clarky987');
