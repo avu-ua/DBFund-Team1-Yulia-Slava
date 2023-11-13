9aa1d65d068511c34ff28e08c7cc1ca2dfeeca29
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2023-11-13 13:27:22 +0200

    Update insert queries.sql

--- insert queries.sql
+++ insert queries.sql
@@ -27,3 +27,35 @@ VALUES ('William', 'Moore', 'william.moore@email.com', '2020-07-03', 'password78
 
 INSERT INTO users (first_name, last_name, user_email, registration_date, password)
 VALUES ('Ava', 'Clark', 'ava.clark@email.com', '2021-12-30', 'clarky987');
+
+
+
+INSERT INTO compositions (title, upload_date, file_format, duration, text, access_restriction)
+VALUES ('Song 1', '2020-01-15', 'mp3', '03:45:00', 'This is the first song about something.', 'unrestricted');
+
+INSERT INTO compositions (title, upload_date, file_format, duration, text, access_restriction)
+VALUES ('Jazz Improv', '2020-03-22', 'wav', '05:20:00', 'An improvisational jazz piece.', 'all registered users');
+
+INSERT INTO compositions (title, upload_date, file_format, duration, text, access_restriction)
+VALUES ('Rock Anthem', '2021-05-10', 'mp3', '04:10:00', 'A powerful rock anthem.', 'all registered users');
+
+INSERT INTO compositions (title, upload_date, file_format, duration, text, access_restriction)
+VALUES ('Electronic Beat', '2021-08-07', 'wav', '03:30:00', 'A catchy electronic beat.', 'all registered users');
+
+INSERT INTO compositions (title, upload_date, file_format, duration, text, access_restriction)
+VALUES ('Folk Ballad', '2022-02-28', 'mp3', '04:55:00', 'A heartfelt folk ballad.', 'all registered users');
+
+INSERT INTO compositions (title, upload_date, file_format, duration, text, access_restriction)
+VALUES ('Latin Rhythm', '2022-06-12', 'wav', '03:15:00', 'A lively Latin rhythm.', 'unrestricted');
+
+INSERT INTO compositions (title, upload_date, file_format, duration, text, access_restriction)
+VALUES ('Bluesy Tune', '2023-09-05', 'mp3', '04:40:00', 'A soulful bluesy tune.', 'unrestricted');
+
+INSERT INTO compositions (title, upload_date, file_format, duration, text, access_restriction)
+VALUES ('Acoustic Serenade', '2023-11-18', 'wav', '02:50:00', 'A soothing acoustic serenade.', 'author''s selection');
+
+INSERT INTO compositions (title, upload_date, file_format, duration, text, access_restriction)
+VALUES ('Hip Hop Groove', '2020-07-03', 'mp3', '03:20:00', 'A cool hip hop groove.', 'all registered users');
+
+INSERT INTO compositions (title, upload_date, file_format, duration, text, access_restriction)
+VALUES ('Dance Party', '2021-12-30', 'wav', '06:00:00', 'Get ready to dance all night.', 'unrestricted');
