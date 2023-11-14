cd58c7ad87c45835ec7f06d1a9f9f5470a1feb20
avu-ua <avuua16@gmail.com>
2023-11-14 14:46:26 +0100

    added INSERTS to playlists, limited_access_rights and commentaries

---
+++ commentaries-Slava.sql
@@ -0,0 +1,17 @@
+INSERT INTO commentaries (user_id, composition_id, comment_date, comment_text)
+VALUES (16, 15, '2023-10-02', 'What a thrilling composition! Can author clarify where can I buy a bottle of that "Pagarizantalli" wine?');
+
+INSERT INTO commentaries (parent_comment_id, user_id, composition_id, comment_date, comment_text)
+VALUES (1, 12, 15, '2023-10-04', 'Wow, many thanks! My dear, ANY wine in Georgia is beatiful and will make you horizontal soon enough. But the idea is great, perhaps a wine with such trademark will soon appear on the shelves');
+
+INSERT INTO commentaries (user_id, composition_id, comment_date, comment_text)
+VALUES (14, 14, '2023-03-17', 'Why would any one want to write or listen a song about that moron?');
+
+INSERT INTO commentaries (parent_comment_id, user_id, composition_id, comment_date, comment_text)
+VALUES (2, 19, 14, '2023-03-21', 'Hey man, this is a satirical composition, don''t you get it?');
+
+INSERT INTO commentaries (user_id, composition_id, comment_date, comment_text)
+VALUES (14, 19, '2023-11-09', 'Anybody, do you like this composition? It''s been written by me');
+
+INSERT INTO commentaries (user_id, composition_id, comment_date, comment_text)
+VALUES (16, 18, '2023-10-31', 'Wonderful composition! Makes me feel happy!');
---
+++ playlists-limited-access-Slava.sql
@@ -0,0 +1,34 @@
+INSERT INTO playlists (composition_id, user_id)
+VALUES 
+    (19, 16)
+    , (12, 16)
+    , (13, 16)
+    , (15, 16)
+    , (19, 16)
+    , (20, 16);
+
+INSERT INTO playlists (composition_id, user_id)
+VALUES 
+    (15, 12)
+    , (12, 12)
+    , (17, 12)
+    , (19, 12)
+    , (20, 12);
+    
+INSERT INTO limited_access_rights (composition_id, user_id)
+VALUES
+    (15, 12)
+    , (15, 11)
+    , (15, 14)
+    , (15, 16)
+    , (15, 18)
+    , (15, 19)
+    , (15, 20);
+
+INSERT INTO limited_access_rights (composition_id, user_id)
+VALUES
+    (16, 13)
+    , (16, 11)
+    , (16, 14)
+    , (16, 17)
+    , (16, 19);
