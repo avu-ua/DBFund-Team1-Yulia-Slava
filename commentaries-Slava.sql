e819068bbc321032bd176cd69b8a31d75d6540da
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2023-11-15 13:04:08 +0200

    Update commentaries-Slava.sql

--- commentaries-Slava.sql
+++ commentaries-Slava.sql
@@ -15,3 +15,24 @@ VALUES (14, 19, '2023-11-09', 'Anybody, do you like this composition? It''s been
 
 INSERT INTO commentaries (user_id, composition_id, comment_date, comment_text)
 VALUES (16, 18, '2023-10-31', 'Wonderful composition! Makes me feel happy!');
+
+
+
+
+INSERT INTO commentaries (user_id, composition_id, comment_date, comment_text)
+VALUES (5, 1, '2020-01-20', 'This track is pure magic! I can''t stop listening to it.');
+
+INSERT INTO commentaries (parent_comment_id, user_id, composition_id, comment_date, comment_text)
+VALUES (7, 3, 1, '2020-01-25', 'Thank you so much! I''m thrilled you find the track magical. Your support means the world to me.');
+
+INSERT INTO commentaries (user_id, composition_id, comment_date, comment_text)
+VALUES (7, 3, '2021-05-17', 'The lyrics speak to my soul. This track is an emotional journey.');
+
+INSERT INTO commentaries (parent_comment_id, user_id, composition_id, comment_date, comment_text)
+VALUES (9, 5, 3, '2021-05-21', 'It warms my heart to hear that the lyrics connect with you. Music is a universal language, and I''m grateful for the connection.');
+
+INSERT INTO commentaries (user_id, composition_id, comment_date, comment_text)
+VALUES (9, 10, '2022-01-02', 'The beats in this song are absolutely infectious. It''s my new favorite!');
+
+INSERT INTO commentaries (user_id, composition_id, comment_date, comment_text)
+VALUES (8, 10, '2022-01-05', 'This is the perfect song to unwind and relax. It has a calming vibe that I love.');
