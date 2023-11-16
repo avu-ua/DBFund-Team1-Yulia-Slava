3c1ac806d24a74cad704066181c86767a9ac5210
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2023-11-16 10:08:01 +0200

    Update View.sql

--- Task 10 - Views/View.sql
+++ Task 10 - Views/View.sql
@@ -1,6 +1,12 @@
-horizontal
+--horizontal
 
 CREATE VIEW track_for_all_users AS
 SELECT *
 FROM compositions
 WHERE access_restriction = 'all registered users'
+
+--vertical
+  
+CREATE VIEW users_names AS
+SELECT user_id, first_name, last_name
+FROM users
