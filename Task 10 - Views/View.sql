b7cbf6a08518b7e1778f09364664e6de51df82df
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2023-11-16 11:35:19 +0200

    Update View.sql

--- Task 10 - Views/View.sql
+++ Task 10 - Views/View.sql
@@ -10,3 +10,10 @@ WHERE access_restriction = 'all registered users'
 CREATE VIEW users_names AS
 SELECT user_id, first_name, last_name
 FROM users
+
+--grouped
+  
+CREATE VIEW user_registration_count AS
+SELECT DATE_TRUNC('year', registration_date) AS registration_year, COUNT(*) AS user_count
+FROM users
+GROUP BY registration_year;
