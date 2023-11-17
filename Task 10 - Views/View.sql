8b9f961f7394b33c9379b4ecefde037f8c5428e3
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2023-11-17 11:38:53 +0200

    Update View.sql

--- Task 10 - Views/View.sql
+++ Task 10 - Views/View.sql
@@ -11,9 +11,59 @@ CREATE VIEW users_names AS
 SELECT user_id, first_name, last_name
 FROM users
 
---grouped
+--mixed
   
-CREATE VIEW user_registration_count AS
-SELECT DATE_TRUNC('year', registration_date) AS registration_year, COUNT(*) AS user_count
-FROM users
-GROUP BY registration_year;
+CREATE VIEW mp3_compositions AS
+SELECT title, duration, text
+FROM compositions
+WHERE file_format LIKE 'mp3';
+
+--with joining
+
+CREATE VIEW authors_name_comp AS   
+   SELECT
+    a.user_id AS author_id,
+    u.first_name,
+    u.last_name,
+    c.title AS composition_title
+    FROM authors a
+    JOIN users u ON a.user_id = u.user_id
+    JOIN compositions c ON a.composition_id = c.composition_id
+
+--with subquery
+
+ CREATE VIEW rock_songs AS
+ SELECT c.title
+ FROM compositions c 
+ WHERE c.composition_id in (
+       SELECT g.composition_id
+ 	     FROM genres g 
+	     WHERE genre_name = 'rock')
+
+--with UNION
+
+CREATE VIEW all_comments AS
+	SELECT comment_text
+	FROM commentaries
+	WHERE parent_comment_id IS NULL
+	UNION
+	SELECT comment_text
+	FROM commentaries
+	WHERE parent_comment_id IS NOT NULL
+
+--view on the select from another one view
+
+CREATE VIEW james_songs AS	
+	SELECT first_name, last_name, composition_title
+	FROM authors_name_comp
+	WHERE first_name = 'James'
+
+--view with check option
+  
+CREATE VIEW long_duration_compositions AS
+SELECT *
+FROM compositions
+WHERE duration > '05:00:00'
+WITH CHECK OPTION
+
+
