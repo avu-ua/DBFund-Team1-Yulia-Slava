9be24ff8753f91906f6407f1f5f893d16249960a
Yuliia Shaposhnikova <81677984+YuliaShap@users.noreply.github.com>
2023-11-21 11:18:19 +0200

    Update Functions.sql

--- Task 11 - Functions-Triggers/Functions.sql
+++ Task 11 - Functions-Triggers/Functions.sql
@@ -1,28 +1,32 @@
-CREATE FUNCTION count_hip_hop_compositions()
-RETURNS INTEGER AS
+CREATE OR REPLACE FUNCTION count_compositions_by_genre(genre_name_param VARCHAR)
+RETURNS INTEGER AS $$
 BEGIN
     RETURN (
         SELECT COUNT(*)
         FROM compositions c
-        JOIN genres g ON c.genre_id = g.genre_id
-        WHERE g.genre_name = 'hip-hop'
+        JOIN genres g ON c.composition_id = g.composition_id
+        WHERE g.genre_name = genre_name_param
     );
 END;
+$$ LANGUAGE plpgsql;
 
 
-CREATE FUNCTION count_registered_users_2020()
-RETURNS INTEGER AS
+
+CREATE OR REPLACE FUNCTION count_registered_users_by_year(registration_year_param INTEGER)
+RETURNS INTEGER AS $$
 BEGIN
     RETURN (
         SELECT COUNT(*)
         FROM users
-        WHERE DATE_PART('year', registration_date) = 2020
+        WHERE EXTRACT(YEAR FROM registration_date) = registration_year_param
     );
 END;
+$$ LANGUAGE plpgsql;
 
 
-CREATE FUNCTION find_last_uploaded_composition_title()
-RETURNS TABLE (title VARCHAR) AS
+
+CREATE OR REPLACE FUNCTION find_last_uploaded_composition_title()
+RETURNS TABLE (title VARCHAR) AS $$
 BEGIN
     RETURN QUERY
         SELECT title
@@ -30,3 +34,5 @@ BEGIN
         ORDER BY upload_date DESC
         LIMIT 1;
 END;
+$$ LANGUAGE plpgsql;
+
