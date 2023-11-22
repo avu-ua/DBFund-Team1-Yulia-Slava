2766565d55699b5020254fb43b3564082aa0da66
avu-ua <avuua16@gmail.com>
2023-11-22 17:46:14 +0100

    Second trigger created

--- Task 11 - Functions-Triggers/Triggers.sql
+++ Task 11 - Functions-Triggers/Triggers.sql
@@ -5,17 +5,17 @@
 CREATE OR REPLACE FUNCTION update_deleted_genre()
 RETURNS TRIGGER AS $$
 BEGIN
-IF NOT EXISTS (
-    SELECT 1
-    FROM genres
-    WHERE composition_id = OLD.composition_id
-    AND genre_name <> OLD.genre_name
-    ) THEN
-    INSERT INTO genres (composition_id, genre_name)
-    VALUES (OLD.composition_id, 'unclassified');
-END IF;
+    IF NOT EXISTS (
+        SELECT 1
+        FROM genres
+        WHERE composition_id = OLD.composition_id
+        AND genre_name <> OLD.genre_name
+        ) THEN
+        INSERT INTO genres (composition_id, genre_name)
+        VALUES (OLD.composition_id, 'unclassified');
+    END IF;
 
-RETURN OLD;
+    RETURN OLD;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -24,3 +24,29 @@ CREATE OR REPLACE TRIGGER before_delete_genre
 BEFORE DELETE ON genres
 FOR EACH ROW
 EXECUTE FUNCTION update_deleted_genre();
+
+
+-- TRIGGER 2 - ADDS UNDERSCORE CHARACTER IN TITLE OF THE NEWLY INSERTED COMPOSITION
+-- IF IT IS DUPLICATIVE OF ALREADY EXISTING TITLE 
+
+-- Function called by the trigger
+CREATE OR REPLACE FUNCTION update_duplicate_title()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF EXISTS (
+        SELECT *
+        FROM compositions
+        WHERE title = NEW.title
+    ) THEN
+        NEW.title = NEW.title || '_';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Before- trigger
+CREATE OR REPLACE TRIGGER before_insert_composition
+BEFORE INSERT ON compositions
+FOR EACH ROW
+EXECUTE FUNCTION update_duplicate_title();
